1bcfdb18abfb5ec3f15c256dd24adfb94b457082
Helene Haugen <helene.e.h@hotmail.com>
2020-02-13 10:02:31 +0100

    update

---
+++ source/driver/lights.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hardware.h"
+#include "lights.h"
+
+void set_order_light_on(){
+
+	
+	for (int i=0; i < HARDWARE_NUMBER_OF_FLOORS; i++) {
+			if (hardware_read_order(i, HARDWARE_ORDER_UP)) {
+				hardware_command_order_light(i, HARDWARE_ORDER_UP, 1);
+			}
+			if (hardware_read_order(i, HARDWARE_ORDER_DOWN)) {
+				hardware_command_order_light(i, HARDWARE_ORDER_DOWN, 1);
+			}
+			if (hardware_read_order(i, HARDWARE_ORDER_INSIDE)) {
+				hardware_command_order_light(i, HARDWARE_ORDER_INSIDE, 1);
+			}
+
+
+	}
+	
+}
+
+void set_order_light_off(){
+	for (int i=0; i < HARDWARE_NUMBER_OF_FLOORS; i++) {
+		if (hardware_read_floor_sensor(i)) {
+			hardware_command_order_light(i, HARDWARE_ORDER_UP, 0);
+			hardware_command_order_light(i, HARDWARE_ORDER_DOWN, 0);
+			hardware_command_order_light(i, HARDWARE_ORDER_INSIDE, 0);
+		}
+	}
+}
+
+void set_floor_light(){
+	for(int i = 0; i < HARDWARE_NUMBER_OF_FLOORS; i++){
+		if (hardware_read_floor_sensor(i)) {
+			hardware_command_floor_indicator_on(i);
+		}
+	}
+}
+
+
+void set_stop_light(){
+	if (hardware_read_stop_signal()) {
+		hardware_command_stop_light(1);
+	}
+}
+
---
+++ source/lights.h
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * @brief Module for lights.
+ *
+ *
+ * Pauline M. Jonassen & Helene E. Haugen
+ */
+
+
+
+/**
+ * @brief checks if any order buttons have been pushed and turns on the light in the pushed button
+ */
+void set_order_light_on();
+
+/**
+ * @brief checks floor sensors and turns on the floor indicator light, the light switches when another floor sensor is active 
+ */
+void set_floor_light();
+
+/**
+ * @brief checks if the stop button is pushed, and turns on the light in the stop button if so
+ */
+void set_stop_light();
+
+/**
+ * @brief turns of order light if the elevator is in right floor
+ */
+void set_order_light_off();
